e44e0a83af3a95e9bf74a318322b40f640783f81
Angga Ari Wijaya <anggadarkprince@gmail.com>
2023-07-02 22:08:14 +0700

    feat(data-fetching): add get static props

---
+++ next-data-fetching/pages/index.tsx
@@ -0,0 +1,44 @@
+import {Inter} from 'next/font/google'
+import fs from 'fs/promises';
+import path from "path";
+
+const inter = Inter({subsets: ['latin']})
+
+type Product = {
+  id: string;
+  title: string;
+}
+
+export default function Home({products}: { products: Array<Product> }) {
+  return (
+    <main className={inter.className}>
+      <ul>
+        {products.map((product: Product) => <li key={product.id}>{product.title}</li>)}
+      </ul>
+    </main>
+  )
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const products = await fs.readFile(filePath);
+  const data: any = JSON.parse(products.toString());
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
+  if (data.length === 0) {
+    return {notFound: true};
+  }
+
+  return {
+    props: {
+      products: data.products
+    },
+    revalidate: 15,
+  }
+}
