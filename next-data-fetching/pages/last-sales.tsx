bc35978b805ecdfacd2c9ddcaab6de16e85f0b6e
Angga Ari Wijaya <anggadarkprince@gmail.com>
2023-07-08 09:27:12 +0700

    add swr as fetcher and provide static solution

--- next-data-fetching/pages/last-sales.tsx
+++ next-data-fetching/pages/last-sales.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import useSWR from 'swr';
 
 interface Product {
     id: number,
@@ -6,11 +7,21 @@ interface Product {
     price: number,
     quantity: number,
 }
-function LastSalesPage() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [products, setProducts] = useState([]);
+
+function LastSalesPage(props: {products: Product[]}) {
+    //const [isLoading, setIsLoading] = useState(true);
+    const [products, setProducts] = useState<Product[]>(props.products);
+
+    const {data, error, isLoading} = useSWR('https://dummyjson.com/carts/1', (url) => fetch(url).then(res => res.json()));
 
     useEffect(() => {
+        if (data) {
+            const sales = data.products.reduce((total: number, item: {price: number}) => total + item.price, 0);
+            setProducts(data.products);
+        }
+    }, [data]);
+
+    /*useEffect(() => {
         setIsLoading(true);
         fetch('https://dummyjson.com/carts/1')
             .then(response => response.json())
@@ -19,21 +30,33 @@ function LastSalesPage() {
                 setProducts(cart.products);
                 setIsLoading(false);
             })
-    }, []);
+    }, []);*/
 
-    if (isLoading) {
+    if (error) {
+        return <p>Failed to load.</p>
+    }
+
+    if (isLoading && !products) {
         return <p>Loading...</p>
     }
 
     return (
         <ul>
-            {products.map((item: Product) => (
+            {products && products.map((item: Product) => (
                 <li key={item.id}>
                     {item.title}: ${item.price}
                 </li>
             ))}
         </ul>
     );
+}
+
+export async function getStaticProps() {
+    const response = await fetch('https://dummyjson.com/carts/1');
+    const cart = await response.json();
+    return {
+        props: {products: cart.products},
+    };
 }
 
 export default LastSalesPage;
